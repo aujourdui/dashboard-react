26e127d1b8e8a79b07ceec0304df38f7908e1fa0
kyosuke <kyosuke.ito@gmail.com>
2022-04-04 12:19:00 -0700

    create basic dashboard with styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import { Title } from "./styles/styles";
+// import MenuIcon from "@mui/icons-material/Menu";
+
+import {
+  RelativeContainer,
+  SidebarContainer,
+  MainContainer,
+  MenuIconContainer,
+  CustomMenuIcon,
+} from "./styles/styles";
+// import axios from "axios";
 
 const App = () => {
-  return <Title>Hello world</Title>;
+  return (
+    <>
+      <RelativeContainer>
+        <SidebarContainer>Sidebar</SidebarContainer>
+        <MainContainer>
+          <MenuIconContainer>
+            <CustomMenuIcon />
+          </MenuIconContainer>
+        </MainContainer>
+      </RelativeContainer>
+    </>
+  );
 };
 
 export default App;
+
+{
+  /* 
+<RelativeContainer>
+<SidebarContainer>Sidebar</SidebarContainer>
+<MainContainer></MainContainer>
+<MenuIconContainer></MenuIconContainer>
+</RelativeContainer> */
+}
