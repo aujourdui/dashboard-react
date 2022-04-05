2cec7cc49305ad994fa7dc1b278895ea5b978dcc
kyosuke <kyosuke.ito@gmail.com>
2022-04-05 15:08:18 -0700

    try to keep css transition code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 import {
   RelativeContainer,
@@ -11,17 +12,54 @@ import {
 
 import Dashboard from "./Dashboard";
 
+// const transitionStyle = {
+//   entering: {
+//     transition: "all 1s ease",
+//     transform: "-translateX(100%)",
+//   },
+//   entered: {
+//     transition: "all 1s ease",
+//     transform: "-translateX(100%)",
+//   },
+//   exiting: {
+//     transition: "all 1s ease",
+//     transform: "translateX(0)",
+//   },
+//   exited: {
+//     transition: "all 1s ease",
+//     transform: "translateX(0)",
+//   },
+// };
+
 const App = () => {
   const [show, setShow] = useState(true);
+  // const [mount, setMount] = useState(false);
 
   const toggleSidebar = () => {
     setShow(!show);
+    // setMount(!mount);
   };
 
   return (
     <>
       <RelativeContainer>
-        {show && <SidebarContainer>Sidebar</SidebarContainer>}
+        {show && (
+          <CSSTransition
+            in={show}
+            timeout={300}
+            classNames="alert"
+            unmountOnExit
+            onEnter={() => setShow(false)}
+            onExited={() => setShow(true)}
+          >
+            <div>
+              <SidebarContainer>
+                <p>Sidebar</p>
+              </SidebarContainer>
+            </div>
+          </CSSTransition>
+        )}
+
         {show ? (
           <>
             <MobileContainer onClick={toggleSidebar}></MobileContainer>
@@ -46,3 +84,17 @@ const App = () => {
 };
 
 export default App;
+
+{
+  /* <Transition in={mount} timeout={1000}>
+{(state) => (
+  <div style={transitionStyle[state]}>
+    <SidebarContainer>
+      <p>Sidebar</p>
+      <p>{mount ? "in=true" : "in=false"}</p>
+      <p>{state}</p>
+    </SidebarContainer>
+  </div>
+)}
+</Transition> */
+}
