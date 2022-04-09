992793706eab713a86b7f3a00c5a06ac6b066974
kyosuke <kyosuke.ito@gmail.com>
2022-04-09 10:46:20 -0700

    add css grid feature much less code

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,19 +3,9 @@ import React, { useState, useEffect } from "react";
 import {
   MainContentsContainer,
   TaskContainer,
-  TaskContentContainer,
-  TaskSection,
   TaskItem,
-  TaskContent,
   TaskAvatarImage,
   TaskAvatarContainer,
-  TaskItemContainer,
-  TaskAssignee,
-  TaskDue,
-  TaskPriority,
-  TaskItemAssignee,
-  TaskItemDue,
-  TaskItemPriority,
 } from "../styles/styles";
 
 import * as api from "../api/index";
@@ -52,114 +42,31 @@ const TaskList = () => {
     <MainContentsContainer>
       TaskList
       <TaskContainer>
-        <TaskContentContainer>Content</TaskContentContainer>
-        <TaskAssignee>Assignee</TaskAssignee>
-        <TaskDue>Due</TaskDue>
-        <TaskPriority>Priority</TaskPriority>
+        <div>Content</div>
+        <div>Assignee</div>
+        <div>Due</div>
+        <div>Priority</div>
         <TaskItem>Finish the mid-term project</TaskItem>
-        <TaskItemAssignee>
+        <TaskItem>
           <TaskAvatarContainer>
             <TaskAvatarImage src="/hero-image.jpeg" />
             Kyosuke
           </TaskAvatarContainer>
-        </TaskItemAssignee>
-        <TaskItemDue>Today</TaskItemDue>
-        <TaskItemPriority>High</TaskItemPriority>
+        </TaskItem>
+        <TaskItem>Today</TaskItem>
+        <TaskItem>High</TaskItem>
         <TaskItem>Study in the central library for 8hours</TaskItem>
-        <TaskItemAssignee>
+        <TaskItem>
           <TaskAvatarContainer>
             <TaskAvatarImage src="https://media-exp1.licdn.com/dms/image/D5635AQFxVf3purrRog/profile-framedphoto-shrink_200_200/0/1649122686962?e=1649444400&v=beta&t=sfF1kIOjR4gafv0m0-SPnPoLVtnrSYX8bqJBnbsNWwE" />
             Sho
           </TaskAvatarContainer>
-        </TaskItemAssignee>
-        <TaskItemDue>Every day</TaskItemDue>
-        <TaskItemPriority>High</TaskItemPriority>
-      </TaskContainer>
-      {/* <TaskContainer>
-
-      </TaskContainer> */}
-      {/* <TaskItemContainer>
-        <TaskItem>
-          Study in the central library for 8hours
         </TaskItem>
-        <TaskSection>Sho</TaskSection>
-        <TaskSection>Every Day</TaskSection>
-        <TaskSection>Hight</TaskSection>
-      </TaskItemContainer> */}
-      {/* <TaskContainer>
-        <TaskContentContainer>Content</TaskContentContainer>
-        <TaskSection>Assignee</TaskSection>
-        <TaskSection>Due</TaskSection>
-        <TaskSection>Priority</TaskSection>
-      </TaskContainer>
-      <TaskItemContainer>
-        <TaskContentContainer>Finish the midterm Project</TaskContentContainer>
-        <TaskItem>Kyosuke</TaskItem>
-        <TaskItem>Today</TaskItem>
-        <TaskItem>High</TaskItem>
-      </TaskItemContainer>
-      <TaskItemContainer>
-        <TaskContentContainer>
-          Go to Library to study for 8 hours{" "}
-        </TaskContentContainer>
-        <TaskItem>Sho</TaskItem>
         <TaskItem>Every day</TaskItem>
         <TaskItem>High</TaskItem>
-      </TaskItemContainer> */}
+      </TaskContainer>
     </MainContentsContainer>
   );
 };
 
 export default TaskList;
-
-{
-  /* <MainContentsContainer>
-TaskList
-<TaskContainer>
-  {task.map((task) => (
-    <TaskContentContainer key={task.id}>
-      <TaskSection>
-        <TaskAvatarContainer>
-          <TaskAvatarImage
-            src={task.image}
-            alt="avatar"
-          ></TaskAvatarImage>
-          {task.assignee}
-        </TaskAvatarContainer>
-      </TaskSection>
-      <div>{task.due}</div>
-    </TaskContentContainer>
-  ))}
-  <TaskContentContainer>
-    <TaskContent>Content</TaskContent>
-    <TaskItem>{task[0].task}</TaskItem>
-    <TaskItem>{task[1].task}</TaskItem>
-  </TaskContentContainer>
-  <TaskSection>
-    <TaskContent>Assignee</TaskContent>
-    <TaskAssignee>
-      <TaskAvatarContainer>
-        <TaskAvatarImage src={task[0].image}></TaskAvatarImage>
-        {task[0].name}
-      </TaskAvatarContainer>
-    </TaskAssignee>
-    <TaskAssignee>
-      <TaskAvatarContainer>
-        <TaskAvatarImage src={task[1].image}></TaskAvatarImage>
-        {task[1].name}
-      </TaskAvatarContainer>
-    </TaskAssignee>
-  </TaskSection>
-  <TaskSection>
-    <TaskContent>Due</TaskContent>
-    <TaskItem>{task[0].due}</TaskItem>
-    <TaskItem>{task[1].due}</TaskItem>
-  </TaskSection>
-  <TaskSection>
-    <TaskContent>Priority</TaskContent>
-    <TaskItem>{task[0].priority}</TaskItem>
-    <TaskItem>{task[1].priority}</TaskItem>
-  </TaskSection>
-</TaskContainer>
-</MainContentsContainer> */
-}
