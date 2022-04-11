23892a1b848c274c4d8c2d6d7cdbf44961b303e2
kyosuke <kyosuke.ito@gmail.com>
2022-04-11 11:20:43 -0700

    add typescript feature to frontend

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import DashboardPage from "./pages/DashboardPage";
 import TaskListPage from "./pages/TaskListPage";
 import MemberListPage from "./pages/MemberListPage";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const container = document.getElementById("root");
@@ -26,4 +25,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
